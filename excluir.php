58f7e2f5f752e13b866959480ec93f2ae5ba2848
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2020-01-17 09:28:53 -0300

    projeto php

---
+++ excluir.php
@@ -0,0 +1,19 @@
+<?php
+//pegar o id
+$id = $_GET["id"];
+//abrir conexão com banco de dados
+include_once'conexao.php';
+$sql = "delete from produto where idproduto =".$id;
+//igual ao gravar
+if (mysqli_query($con,$sql)){
+    $msg = "Produto excluído com sucesso!";
+} else {
+    $msg = "Erro ao deletar produto.";
+}
+mysqli_close($con);
+
+echo"<script>
+alert('".$msg."');
+location.href='consultar.php';
+</script>";
+?>
